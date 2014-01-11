3c719ecda0a1888c409671832aa8751260b3dac1
Johannes B. Ullrich <jullrich@sans.edu>
2014-01-10 20:36:29 -0500

    misc changes

---
+++ php/footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jullrich
+ * Date: 1/7/14
+ * Time: 8:44 AM
+ */
+?>
+<footer>
+    <a href="list.html">Index</a>
+    <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">
+        <img alt="Creative Commons License"
+                                                                                          style="border-width:0"
+                                                                                          src="http://i.creativecommons.org/l/by-sa/3.0/80x15.png"/></a><br/>
+    <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://purl.org/dc/terms/
+http://purl.org/dc/terms/ ">AuthOnTheMove</span> by <a
+        xmlns:cc="http://creativecommons.org/ns#" href="http://www.authonthemove.com" property="cc:attributionName"
+        rel="cc:attributionURL">the SANS Technology Institute</a> is licensed under a <a rel="license"
+                                                                                         href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative
+    Commons Attribution-ShareAlike 3.0 Unported License</a>.<br/>
+</footer>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jullrich
+ * Date: 1/7/14
+ * Time: 8:45 AM
+ */ 
